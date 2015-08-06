136d65774bf30645ffc553e6baf783c2829c1db8
Darren Tsung <darrentsung@Darrens-MacBook-Pro.local>
2015-08-06 09:04:26 -0700

    Renamed functions to match syntax styling

--- DT/SharedScripts/Util/Vector3Extensions.cs
+++ DT/SharedScripts/Util/Vector3Extensions.cs
@@ -3,42 +3,42 @@ using UnityEngine;
 
 namespace DT {
   public static class Vector3Extensions {
-    public static Vector3 changeXYZ (this Vector3 v, float x, float y, float z) {
+    public static Vector3 ChangeXYZ (this Vector3 v, float x, float y, float z) {
       v.x = x;
       v.y = y;
       v.z = z;
       return v;
     }
     
-    public static Vector3 changeXY (this Vector3 v, float x, float y) {
+    public static Vector3 ChangeXY (this Vector3 v, float x, float y) {
       v.x = x;
       v.y = y;
       return v;
     }
     
-    public static Vector3 changeXZ (this Vector3 v, float x, float z) {
+    public static Vector3 ChangeXZ (this Vector3 v, float x, float z) {
       v.x = x;
       v.z = z;
       return v;
     }
     
-    public static Vector3 changeYZ (this Vector3 v, float y, float z) {
+    public static Vector3 ChangeYZ (this Vector3 v, float y, float z) {
       v.y = y;
       v.z = z;
       return v;
     }
     
-    public static Vector3 changeX (this Vector3 v, float x) {
+    public static Vector3 ChangeX (this Vector3 v, float x) {
       v.x = x;
       return v;
     }
     
-    public static Vector3 changeY (this Vector3 v, float y) {
+    public static Vector3 ChangeY (this Vector3 v, float y) {
       v.y = y;
       return v;
     }
     
-    public static Vector3 changeZ (this Vector3 v, float z) {
+    public static Vector3 ChangeZ (this Vector3 v, float z) {
       v.z = z;
       return v;
     }
